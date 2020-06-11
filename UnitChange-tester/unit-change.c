865e2677cb39e7b9756380b31e94de6e53876e0e
lielie5 <50637332+lielie5@users.noreply.github.com>
2020-06-12 00:06:55 +0900

    revise atm_torr, lbf_ft__J / add program ending

--- UnitChange-tester/unit-change.c
+++ UnitChange-tester/unit-change.c
@@ -8,14 +8,14 @@ const double lbm_kg = 0.4536;
 const double gal_inch = 231;
 const double psi_atm = 14.949;
 const double atm_bar = 1.01325;
-const double atm_torr = 4.4482;
+const double atm_torr = 760;
 
 const double BTU_J = 1055;
 const double cal_J = 4.184;
 const double HP_W = 745;
  
 const double lbf_N = 4.4482;
-const double lbf_ft__J = 1.343;
+const double lbf_ft__J = 1.354;
 const double atm_ft3__BTU = 2.7195;
 
 const double when_0C_R = 491.67;
@@ -45,6 +45,7 @@ int main()
 {
 	int i = 0;
 	int value = 0;
+	char *temp_enter;
 	double temp;
 	srand(time(NULL));
 	while (value == 0)
@@ -108,5 +109,8 @@ int main()
 	
 	printf("Feedback is always welcome.\n");
 	printf("Contact me dldbstjd5@naver.com OR https://github.com/exgs/hongikuniv_chemical-engineering\n");
+	printf("Input [bye] to exit\n");
+	scanf("%s",&temp_enter);
+	printf("----------------%s------------------\n",&temp_enter);
 	// print_const();
 }
